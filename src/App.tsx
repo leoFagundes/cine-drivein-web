9176e8bc9cb775d1f4f0ac9488ab28dc0565bf4e
Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>
2024-01-21 19:21:37 -0300

    feat: create input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
 import Router from './routes';
+import {Input} from './Components/Atoms/Input/Input';
+import './Global.scss';
 
 function App() {
+
   return (
-    <Router />
+      <>
+          <Input errorLabel="Salve familia" type='password' onChange={() => console.log('')} placeholder={'Naruto'} />
+          <Router />
+      </>
   );
 }
 
---
+++ src/Components/Atoms/Input/Input.tsx
@@ -0,0 +1,34 @@
+import styles from './Input.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
+import {useState} from "react";
+
+type InputType = {
+    value?: string;
+    placeholder: string;
+    onChange: VoidFunction;
+    errorLabel?: string;
+    type?: string;
+}
+export const Input = ({value, placeholder, onChange, errorLabel, type}: InputType) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+    const handlePassword = () => {
+        setIsPasswordVisible(!isPasswordVisible)
+    }
+
+    const IS_ERROR_INPUT_STYLE = errorLabel ? styles.inputContainer__isNegative : styles.inputContainer;
+    const IS_PASSWORD_VISIBLE_ICON = isPasswordVisible ? faEye : faEyeSlash;
+    const IS_PASSWORD_VISIBLE_TYPE = isPasswordVisible ? undefined : type;
+    return (
+        <div className={styles.container}>
+            <input type={IS_PASSWORD_VISIBLE_TYPE} value={value} placeholder={placeholder} onChange={onChange} className={IS_ERROR_INPUT_STYLE}/>
+            {type === 'password' &&
+                <div aria-label='icone de olho' onClick={handlePassword} className={styles.icon}>
+                    <FontAwesomeIcon size='lg' icon={IS_PASSWORD_VISIBLE_ICON}/>
+                </div>
+            }
+            {errorLabel && <label className={styles.labelError}>{errorLabel}</label>}
+        </div>
+    )
+}
