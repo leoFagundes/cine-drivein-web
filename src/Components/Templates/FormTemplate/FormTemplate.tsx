97189c92d2b94f2aadf484ea89063b7927f8ed9a
leoFagundes <leofagundes2015@gmail.com>
2024-01-24 13:02:21 -0300

    feat: Update templates

--- src/Components/Templates/FormTemplate/FormTemplate.tsx
+++ src/Components/Templates/FormTemplate/FormTemplate.tsx
@@ -1,40 +1,51 @@
 import styles from './FormTemplate.module.scss';
 import LogoImage from "../../Atoms/LogoImage";
 import Text from "../../Atoms/Text";
-import {Input} from "../../Atoms/Input/Input";
+import { Input } from "../../Atoms/Input/Input";
 import Button from "../../Atoms/Button";
+import { ChangeEvent } from 'react';
 
 type FormTemplateType = {
     label: string,
-    inputs: [{
+    inputs: Array<{
         value: string;
         placeholder: string;
-        onChange: VoidFunction;
+        onChange: (e: ChangeEvent<HTMLInputElement>) => void;
         type?: string;
-    }]
+        errorLabel: string;
+    }>;
     buttonLabel: string;
-    onClick: VoidFunction;
+    buttonOnClick: VoidFunction;
     linkLabel: string;
+    linkIcon: React.ReactNode;
+    linkOnClick: VoidFunction;
+
 }
 
-export const FormTemplate = ({ label, inputs, buttonLabel, onClick, linkLabel }: FormTemplateType) => {
+export const FormTemplate = ({ label, inputs, buttonLabel, buttonOnClick, linkLabel, linkIcon, linkOnClick }: FormTemplateType) => {
     return (
         <section className={styles.container}>
-            <LogoImage/>
+            <LogoImage />
             <Text fontSize='extraLarge'>{label}</Text>
-            {
-                inputs.map((item) => {
-                    return <Input
-                        key={item.placeholder}
-                        value={item.value}
-                        placeholder={item.placeholder}
-                        onChange={item.onChange}
-                        type={item.type}
-                    />
-                })
-            }
-            <Button label={buttonLabel} onClick={onClick}/>
-            { linkLabel && <Text isLink>{linkLabel}</Text> }
+            <div className={styles.inputs}>
+                {
+                    inputs.map((item) => {
+                        return <Input
+                            key={item.placeholder}
+                            value={item.value}
+                            placeholder={item.placeholder}
+                            onChange={item.onChange}
+                            type={item.type}
+                            errorLabel={item.errorLabel}
+                        />
+                    })
+                }
+            </div>
+            <Button label={buttonLabel} onClick={buttonOnClick} />
+            <div className={styles.linkContainer} onClick={linkOnClick}>
+                {linkIcon && linkIcon}
+                {linkLabel && <Text isLink>{linkLabel}</Text>}
+            </div>
         </section>
     )
 }
