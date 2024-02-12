a211b2a615465e50b853c7512f5890b6964d489a
leoFagundes <leofagundes2015@gmail.com>
2024-02-12 10:54:34 -0300

    update button

--- src/Components/Atoms/Button/index.tsx
+++ src/Components/Atoms/Button/index.tsx
@@ -4,11 +4,36 @@ type Props = {
     onClick: () => void;
     label: string;
     marginTop?: string;
+    marginBottom?: string;
+    backGroundColor?: "primary-color" | "main-white" | "invalid-color" | "success-color" | "gray-color" | "placeholder-color" | "background-color" | "background-secondary-color"
 };
 
-export default function Button({ onClick, label, marginTop }: Props) {
+export default function Button({ onClick, label, marginTop, marginBottom, backGroundColor = 'primary-color' }: Props) {
+    const backGroundColorClass = {
+        'primary-color': style.backgroundPrimaryColor,
+        'main-white': style.backgroundMainWhite,
+        'invalid-color': style.backgroundInvalidColor,
+        'success-color': style.backgroundSuccessColor,
+        'gray-color': style.backgroundGrayColor,
+        'placeholder-color': style.backgroundPlaceholderColor,
+        'background-color': style.backgroundBackgroundColor,
+        'background-secondary-color': style.backgroundBackgroundSecondaryColor
+    }[backGroundColor];
+
+    const borderClass = {
+        'primary-color': style.borderPrimaryColor,
+        'main-white': style.borderMainWhite,
+        'invalid-color': style.borderInvalidColor,
+        'success-color': style.borderSuccessColor,
+        'gray-color': style.borderGrayColor,
+        'placeholder-color': style.borderPlaceholderColor,
+        'background-color': style.borderBackgroundColor,
+        'background-secondary-color': style.borderBackgroundSecondaryColor
+    }[backGroundColor];
+
+
     return (
-        <button style={{marginTop}} className={style.button} onClick={onClick}>
+        <button style={{ marginTop, marginBottom }} className={`${style.button} ${backGroundColorClass} ${borderClass}`} onClick={onClick}>
             {label}
         </button>
     )
