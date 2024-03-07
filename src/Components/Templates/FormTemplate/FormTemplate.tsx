77513ae9ec9ced102d44e979606621dd3bba8b1d
leoFagundes <leofagundes2015@gmail.com>
2024-03-07 07:13:19 -0300

    feat: implement shopping cart functionality

--- src/Components/Templates/FormTemplate/FormTemplate.tsx
+++ src/Components/Templates/FormTemplate/FormTemplate.tsx
@@ -1,57 +1,69 @@
-import styles from './FormTemplate.module.scss';
+import styles from "./FormTemplate.module.scss";
 import LogoImage from "../../Atoms/LogoImage";
 import Text from "../../Atoms/Text";
 import { Input } from "../../Atoms/Input/Input";
 import Button from "../../Atoms/Button";
-import { ChangeEvent, ReactNode } from 'react';
-import Caption from '../../Molecules/Caption';
+import { ChangeEvent, ReactNode } from "react";
+import Caption from "../../Molecules/Caption";
 
 type FormTemplateType = {
-    label: string,
-    inputs: Array<{
-        value: string;
-        placeholder: string;
-        onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-        type?: string;
-        errorLabel: string;
-        caption?: ReactNode;
-    }>;
-    buttonLabel: string;
-    buttonOnClick: VoidFunction;
-    linkLabel: string;
-    linkIcon: React.ReactNode;
-    linkOnClick: VoidFunction;
+  label: string;
+  inputs: Array<{
+    value: string;
+    placeholder: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+    errorLabel: string;
+    caption?: ReactNode;
+  }>;
+  buttonLabel: string;
+  buttonOnClick: VoidFunction;
+  linkLabel: string;
+  linkIcon: React.ReactNode;
+  linkOnClick: VoidFunction;
+  alert?: ReactNode;
+};
 
-}
-
-export const FormTemplate = ({ label, inputs, buttonLabel, buttonOnClick, linkLabel, linkIcon, linkOnClick }: FormTemplateType) => {
-    const getInputMargin = (index: number, errorLabel: string) => {
-        if (index === 0) return '40px'
-        if (errorLabel) return '12px'
-        return '24px'
-    }
-    return (
-        <section className={styles.container}>
-            <LogoImage marginTop='32px' />
-            <Text marginTop="40px" fontSize='mediumLarge'>{label}</Text>
-            <div className={styles.inputs}>
-                {
-                    inputs.map((item, index) => {
-                        return <Input
-                            key={item.placeholder}
-                            value={item.value}
-                            placeholder={item.placeholder}
-                            onChange={item.onChange}
-                            type={item.type}
-                            errorLabel={item.errorLabel}
-                            marginTop={getInputMargin(index, item.errorLabel)}
-                            caption={item.caption}
-                        />
-                    })
-                }
-            </div>
-            <Button label={buttonLabel} onClick={buttonOnClick} />
-            <Caption onClick={linkOnClick} isLink icon={linkIcon} label={linkLabel} />
-        </section>
-    )
-}
+export const FormTemplate = ({
+  label,
+  inputs,
+  buttonLabel,
+  buttonOnClick,
+  linkLabel,
+  linkIcon,
+  linkOnClick,
+  alert,
+}: FormTemplateType) => {
+  const getInputMargin = (index: number, errorLabel: string) => {
+    if (index === 0) return "40px";
+    if (errorLabel) return "12px";
+    return "24px";
+  };
+  return (
+    <section className={styles.container}>
+      <LogoImage marginTop="32px" />
+      <Text marginTop="40px" fontSize="mediumLarge">
+        {label}
+      </Text>
+      <div className={styles.inputs}>
+        {inputs.map((item, index) => {
+          return (
+            <Input
+              key={item.placeholder}
+              value={item.value}
+              placeholder={item.placeholder}
+              onChange={item.onChange}
+              type={item.type}
+              errorLabel={item.errorLabel}
+              marginTop={getInputMargin(index, item.errorLabel)}
+              caption={item.caption}
+            />
+          );
+        })}
+      </div>
+      <Button label={buttonLabel} onClick={buttonOnClick} />
+      <Caption onClick={linkOnClick} isLink icon={linkIcon} label={linkLabel} />
+      {alert}
+    </section>
+  );
+};
