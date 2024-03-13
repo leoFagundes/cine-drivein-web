064a945ea12bec9636b45638ab501b0892ef4de0
leoFagundes <leofagundes2015@gmail.com>
2024-03-12 21:23:58 -0300

    update close template

--- src/Components/Templates/CloseTemplate/CloseTemplate.tsx
+++ src/Components/Templates/CloseTemplate/CloseTemplate.tsx
@@ -1,39 +1,40 @@
-import styles from './CloseTemplate.module.scss';
+import styles from "./CloseTemplate.module.scss";
 import LogoImage from "../../Atoms/LogoImage";
 import { ReactElement, useState } from "react";
+import Button from "../../Atoms/Button";
 
 type CloseTemplateType = {
-    children: ReactElement;
-}
+  children: ReactElement;
+};
 
 export const CloseTemplate = ({ children }: CloseTemplateType) => {
-    const [randomImage, setRandomImage] = useState("Harley");
+  const [randomImage, setRandomImage] = useState("Harley");
 
-    const getRandomDiceImageString = () => {
-        const length = Math.floor(Math.random() * (8 - 4 + 1)) + 4;
-        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        let randomString = '';
+  const getRandomDiceImageString = () => {
+    const length = Math.floor(Math.random() * (8 - 4 + 1)) + 4;
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let randomString = "";
 
-        for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            randomString += characters.charAt(randomIndex);
-        }
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters.charAt(randomIndex);
+    }
 
-        return randomString;
-    };
+    return randomString;
+  };
 
-    return (
-        <section className={styles.container}>
-            <div className={styles.serviceClose}>
-                <LogoImage marginTop='32px' />
-                {children}
-                <img
-                    onClick={() => setRandomImage(getRandomDiceImageString())}
-                    src={`https://api.dicebear.com/7.x/thumbs/svg?seed=${randomImage}`}
-                    alt="avatar"
-                    className={styles.randomImage}
-                />
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className={styles.container}>
+      <div className={styles.serviceClose}>
+        <LogoImage marginTop="32px" />
+        {children}
+        <img
+          onClick={() => setRandomImage(getRandomDiceImageString())}
+          src={`https://api.dicebear.com/7.x/thumbs/svg?seed=${randomImage}`}
+          alt="avatar"
+          className={styles.randomImage}
+        />
+      </div>
+    </section>
+  );
+};
