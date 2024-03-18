658f5a92dbcdaa92de797f8ce11d5ca7854b6a30
leoFagundes <leofagundes2015@gmail.com>
2024-03-18 19:56:28 -0300

    update details

--- src/Components/Organism/Modal/index.tsx
+++ src/Components/Organism/Modal/index.tsx
@@ -1,27 +1,34 @@
-import styles from './Modal.module.scss';
-import {MouseEvent, ReactNode} from "react";
+import styles from "./Modal.module.scss";
+import { MouseEvent, ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 type ModalType = {
-    isOpen: boolean;
-    onClose: VoidFunction;
-    children: ReactNode;
-}
+  isOpen: boolean;
+  onClose: VoidFunction;
+  children: ReactNode;
+};
 
 export const Modal = ({ children, onClose, isOpen }: ModalType) => {
-    const handleCloseModalWith = (event: MouseEvent)  => {
-        event.preventDefault();
-        event.target === event.currentTarget && onClose();
-    }
-    return (
-        <>
-            {
-                isOpen && <div onClick={handleCloseModalWith} className={styles.container}>
-                    <div className={styles.modalContainer}>
-                        { children }
-                    </div>
-                </div>
-            }
-        </>
-    )
-
-}
+  const handleCloseModalWith = (event: MouseEvent) => {
+    event.preventDefault();
+    event.target === event.currentTarget && onClose();
+  };
+  return (
+    <>
+      {isOpen && (
+        <div onClick={handleCloseModalWith} className={styles.container}>
+          <div className={styles.modalContainer}>
+            <FontAwesomeIcon
+              onClick={onClose}
+              className={styles.closeModalIcon}
+              size="lg"
+              icon={faXmark}
+            />
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
