69cc751cabec7aca27e1c1a5b7989ea1fc3acdcb
leoFagundes <leofagundes2015@gmail.com>
2024-06-07 20:31:54 -0300

    feat: add tests

---
+++ src/Components/Atoms/Dropdown/Dropdown.test.tsx
@@ -0,0 +1,54 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Dropdown } from ".";
+
+const mockFunction = jest.fn();
+
+describe("#Dropdown", () => {
+  test("render correct dropdown", () => {
+    render(
+      <Dropdown
+        options={[]}
+        value=""
+        onChange={mockFunction}
+        placeholder="Categoria"
+      />
+    );
+
+    const dropdown = screen.getByText("Categoria");
+    expect(dropdown).toBeVisible();
+  });
+
+  test("render correct label component if error label exist", () => {
+    render(
+      <Dropdown
+        options={[]}
+        value=""
+        onChange={mockFunction}
+        placeholder="Categoria"
+        errorLabel="error test"
+      />
+    );
+
+    const errorLabel = screen.getByText("error test");
+    expect(errorLabel).toBeVisible();
+  });
+
+  test("renders and handles option clicks correctly", () => {
+    render(
+      <Dropdown
+        options={["option1", "option2"]}
+        value=""
+        onChange={mockFunction}
+        placeholder="Categoria"
+      />
+    );
+
+    const dropdown = screen.getByRole("combobox");
+    fireEvent.change(dropdown, { target: { value: "option1" } });
+
+    expect(mockFunction).toHaveBeenCalledWith("option1");
+
+    expect((dropdown as HTMLSelectElement).value).toBe("option1");
+  });
+});
